ade866ad7c55ab8f7226bc198d1123e819fb5a45
Cong Du <cdu@cdu-ltmdowb.internal.salesforce.com>
2021-07-21 13:24:24 -0700

    update mkdir to handle existing bucket

--- s3fs/core.py
+++ s3fs/core.py
@@ -685,28 +685,31 @@ class S3FileSystem(AsyncFileSystem):
     async def _mkdir(self, path, acl="", create_parents=True, **kwargs):
         path = self._strip_protocol(path).rstrip("/")
         bucket, key, _ = self.split_path(path)
-        if not key or (create_parents and not await self._exists(bucket)):
-            if acl and acl not in buck_acls:
-                raise ValueError("ACL not in %s", buck_acls)
-            try:
-                params = {"Bucket": bucket, "ACL": acl}
-                region_name = kwargs.get("region_name", None) or self.client_kwargs.get(
-                    "region_name", None
-                )
-                if region_name:
-                    params["CreateBucketConfiguration"] = {
-                        "LocationConstraint": region_name
-                    }
-                await self._call_s3("create_bucket", **params)
-                self.invalidate_cache("")
-                self.invalidate_cache(bucket)
-            except ClientError as e:
-                raise translate_boto_error(e)
-            except ParamValidationError as e:
-                raise ValueError("Bucket create failed %r: %s" % (bucket, e))
+        if self._exists(bucket):
+            raise FileExistsError
         else:
-            # raises if bucket doesn't exist, but doesn't write anything
-            await self._ls(bucket)
+            if create_parents:
+                if acl and acl not in buck_acls:
+                    raise ValueError("ACL not in %s", buck_acls)
+                try:
+                    params = {"Bucket": bucket, "ACL": acl}
+                    region_name = kwargs.get("region_name", None) or self.client_kwargs.get(
+                        "region_name", None
+                    )
+                    if region_name:
+                        params["CreateBucketConfiguration"] = {
+                            "LocationConstraint": region_name
+                        }
+                    await self._call_s3("create_bucket", **params)
+                    self.invalidate_cache("")
+                    self.invalidate_cache(bucket)
+                except ClientError as e:
+                    raise translate_boto_error(e)
+                except ParamValidationError as e:
+                    raise ValueError("Bucket create failed %r: %s" % (bucket, e))
+            else:
+                # raises if bucket doesn't exist, but doesn't write anything
+                await self._ls(bucket)
 
     mkdir = sync_wrapper(_mkdir)
 
